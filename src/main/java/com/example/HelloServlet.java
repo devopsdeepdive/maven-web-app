319df0f57681049dcf76bf6fef8f5218d1900693
devopsdeepdive <95200678+devopsdeepdive@users.noreply.github.com>
2026-07-10 10:43:37 +0530

    Create HelloServlet.java

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.IOException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.getWriter().println("Hello Maven Web Application");
+    }
+}
